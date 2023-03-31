29ce4ffe5744b862840593d08327dea90fbe7154
KausaChangs <changayakausa@gmail.com>
2023-03-31 16:52:01 +0200

    added service and all methods to use in application

--- src/main/java/com/example/emoloyeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/example/emoloyeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,11 @@ package com.example.emoloyeemanager.repo;
 import com.example.emoloyeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/example/emoloyeemanager/service/EmployeeService.java
+++ src/main/java/com/example/emoloyeemanager/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.example.emoloyeemanager.service;
 
+import com.example.emoloyeemanager.model.Employee;
 import com.example.emoloyeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
@@ -11,5 +16,29 @@ public class EmployeeService {
     @Autowired
     public EmployeeService(EmployeeRepo employeeRepo) {
         this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(()->new UserNotFoundException
+                        ("User by id "+ id +" was not found."));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
