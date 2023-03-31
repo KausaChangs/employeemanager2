6ab9cf8bcedce7e6c658d7fc4fa37f031d2b2051
KausaChangs <changayakausa@gmail.com>
2023-03-31 15:18:07 +0200

    created EmployeeService class

---
+++ src/main/java/com/example/emoloyeemanager/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.example.emoloyeemanager.service;
+
+import com.example.emoloyeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+}
