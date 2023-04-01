2101d6ce52e9e66a56a4ef3a230d51109890740b
KausaChangs <changayakausa@gmail.com>
2023-04-01 12:15:44 +0200

    created an addEmployee endpoint

--- src/main/java/com/example/emoloyeemanager/EmployeeResource.java
+++ src/main/java/com/example/emoloyeemanager/EmployeeResource.java
@@ -3,10 +3,7 @@ import com.example.emoloyeemanager.model.Employee;
 import com.example.emoloyeemanager.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,7 @@ public class EmployeeResource {
         this.employeeService = employeeService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees(){
         List<Employee> employees = employeeService.findAllEmployee();
                 return new ResponseEntity<>(employees, HttpStatus.OK);
@@ -29,5 +26,11 @@ public class EmployeeResource {
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")Long id){
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
     }
 }
